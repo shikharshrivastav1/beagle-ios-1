8f6dd0ffc82db469ef6b8207d3c557a6b36f6f2f
Shikhar Shrivastav <shikhar2005@gmail.com>
2022-02-18 02:22:43 +0530

    Bug fixes

--- Sources/Beagle/Sources/Components/Widget/Picker/Picker+Extensions.swift
+++ Sources/Beagle/Sources/Components/Widget/Picker/Picker+Extensions.swift
@@ -11,17 +11,20 @@ extension Picker {
     
     public func toView(renderer: BeagleRenderer) -> UIView {
         let picker = BeagleUIPicker()
+        picker.dataSource = picker
+        picker.delegate = picker
         picker.translatesAutoresizingMaskIntoConstraints = false
         if let styleId = styleId {
             picker.beagle.applyStyle(for: picker as UIPickerView, styleId: styleId, with: renderer.controller)
         }
         renderer.observe(entries, andUpdateManyIn: picker) {
             picker.items = $0
+            picker.reloadAllComponents()
         }
-        return picker
+        return AutoLayoutWrapper(view: picker)
     }
     
-    final class BeagleUIPicker: UIPickerView, UIPickerViewDataSource {
+    final class BeagleUIPicker: UIPickerView, UIPickerViewDataSource, UIPickerViewDelegate {
         
         var items: [String]? = nil
         
@@ -30,7 +33,10 @@ extension Picker {
         }
         
         func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            return items?.count ?? 0
+            if component == 0 {
+                return items?.count ?? 0
+            }
+            return 0
         }
         
         func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
